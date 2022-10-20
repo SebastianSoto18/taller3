0e84f7858eeb38cd5edd0d5783f445b536f48c04
SebastianSoto18 <atlasjuanse12@gmail.com>
2022-10-20 01:03:06 -0500

    wdf

--- guia.c
+++ guia.c
@@ -33,6 +33,10 @@ int main(){
             buff[strlen(buff)-1] = '\0';
             printf("[%d]write-->:%s\n",getpid(),buff);
             write(fd[1], buff, strlen(buff));
+            write(fd[1], buff, strlen(buff));
+            write(fd[1], buff, strlen(buff));
+            write(fd[1], buff, strlen(buff));
+            write(fd[1], buff, strlen(buff));
          }
        }while(strcmp(buff,"salir") !=0);
       close(fd[1]);
