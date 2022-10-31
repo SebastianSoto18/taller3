24ceab707fa5efebc4b7e9cde646583450e73cba
SebastianSoto18 <atlasjuanse12@gmail.com>
2022-10-31 00:03:35 -0600

    primer intento matrices con tuverias

---
+++ matricescontuberias.c
@@ -0,0 +1,174 @@
+#include <stdio.h> 
+#include <unistd.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Data{
+    int x;
+    int y;
+    double val;
+};
+
+
+int main(int argc, char **argv){
+
+    struct Data dato;
+
+    int maxtub, **mtub, idx, n,FA,CA,FB,CB;
+    bool flag = true;
+    int i=0;
+    maxtub=3
+
+    do{
+    printf("Ingrese la cantidad de filas de la primera matriz: ");
+    scanf("%d",&FA);
+    printf("Ingrese la cantidad de columnas de la primera matriz: ");
+    scanf("%d",&CA);
+    printf("Ingrese la cantidad de filas de la segunda matriz: ");
+    scanf("%d",&FB);
+    printf("Ingrese la cantidad de columnas de la segunda matriz: ");
+    scanf("%d",&CB);
+
+    if(CA!=FB){
+        printf("Las matrices no son compatibles para la multiplicacion\n");
+        printf("Ingrese nuevamente los datos\n");
+    }
+    else{
+        flag=false;
+
+    }while(flag);
+   
+   int matrizA[FA][CA];
+   int matrizB[FB][CB];
+   int matrizC[FA][CB];
+
+    printf("Ingrese los valores de la primera matriz\n");
+    for(int i=0;i<FA;i++){
+        for(int j=0;j<CA;j++){
+            printf("Ingrese el valor de la posicion [%d][%d]: ",i,j);
+            scanf("%d",&matrizA[i][j]);
+        }
+    }
+
+    printf("Ingrese los valores de la segunda matriz\n");
+    for(int i=0;i<FB;i++){
+        for(int j=0;j<CB;j++){
+            printf("Ingrese el valor de la posicion [%d][%d]: ",i,j);
+            scanf("%d",&matrizB[i][j]);
+        }
+    }
+
+    printf("La primera matriz es:\n");
+    for(int i=0;i<FA;i++){
+        for(int j=0;j<CA;j++){
+            printf("%d ",matrizA[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("La segunda matriz es:\n");
+    for(int i=0;i<FB;i++){
+        for(int j=0;j<CB;j++){
+            printf("%d ",matrizB[i][j]);
+        }
+        printf("\n");
+    }
+
+    mtub = (int **)calloc(maxtub,sizeof(int *));
+
+    for(int f = 0; f < maxtub; f++)
+        mtub[f] = (int *) calloc(2, sizeof(int));
+        
+    for(int tub = 0; tub < maxtub; tub++) pipe(mtub[tub]);
+
+
+
+    for(idx = 0; idx < maxtub; idx++)
+        if(!fork()) break;
+    //bloque de cada proceso
+
+    if(idx == maxtub){ 
+        printf("[%d] padre\n", getpid());
+        for(int tub = 0; tub < maxtub; tub++)
+            close(mtub[tub][1]);
+            
+        while ((n=read(mtub[idx][0], &dato, sizeof(struct Data))) > 0){
+            if(FA*CB < i ){
+                i++;
+            }else{
+                break;
+            }
+            matrizC[dato.x][dato.y] = dato.val;
+        }
+                
+
+        for(int tub = 0; tub < maxtub; tub++)
+            close(mtub[tub][0]);
+
+        for(int chld = 0; chld < maxtub; chld++)
+            wait(NULL);
+        printf("termino padre\n");
+    }
+    else{ //codigo hijo
+    printf("[%d] hijo %d\n", getpid(), idx);
+        for(int tub = 0; tub < maxtub; tub ++){
+            if(tub != idx){
+                close(mtub[tub][0]);
+                close(mtub[tub][1]);
+            }
+        }
+        close(mtub[idx][0]);
+
+
+       switch (idx){
+       {
+       case 0:
+        for(int i=1;i<FA;i++){
+            for(int j=0;j<CB;j++){
+
+                dato.x=i;
+                dato.y=j;
+                dato.val=0;
+                for(int k=0;k<CA;k++){
+                    dato.val+=matrizA[i][k]*matrizB[k][j];
+                }
+            write(mtub[3][1], &dato, sizeof(struct Data));
+            }
+        }
+        break;
+
+         case 1:
+        for(int i=0;i<FA;i++){
+            for(int j=1;j<CB;j++){
+                dato.x=i;
+                dato.y=j;
+                dato.val=0;
+                for(int k=0;k<CA;k++){
+                    dato.val+=matrizA[i][k]*matrizB[k][j];
+                }
+            write(mtub[3][1], &dato, sizeof(struct Data));
+            }
+        }
+        break;
+
+         case 2:
+            for(int i=0;i<FA;i++){
+                dato.x=i;
+                dato.y=i;
+                dato.val=0;
+                for(int k=0;k<CA;k++){
+                    dato.val+=matrizA[i][k]*matrizB[k][i];
+                }
+                write(mtub[3][1], &dato, sizeof(struct Data));
+            }
+        break;
+       
+       }
+
+
+        printf("[%d] termino\n", getpid());
+    }
+    return 0;
+}
