c91d26bf6d21de4e362be56e7a573f878bd655dd
SebastianSoto18 <atlasjuanse12@gmail.com>
2022-10-30 21:00:12 -0600

    primer ejercicio tipo parcial

--- mensajeporvarioshijos.c
+++ mensajeporvarioshijos.c
@@ -69,7 +69,7 @@ int main(){
         while ((n=read(mtub[idx][0], &messageR, strlen(messageR))) > 0){
             printf("%d recibido: %s\n", getpid(), messageR);
 
-            if(strcmp(message, "end") == 0) break;
+            if(strcmp(messageR, "end") == 0) break;
             write(mtub[idx+1][1], &messageR, strlen(messageR));
         }
 
