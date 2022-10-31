ce436c7d30b900fd7cbd57d13c087ff1a792d91c
SebastianSoto18 <atlasjuanse12@gmail.com>
2022-10-30 22:51:36 -0600

    ddw

--- mensajeporvarioshijos.c
+++ mensajeporvarioshijos.c
@@ -39,7 +39,9 @@ int main(){
             printf("[%d]write-->:%s\n",getpid(),buff);
             if(strcmp(buff,"salir")==0){
                 printf("Saliendo del programa\n");
-                write(mtub[0][1], buff, strlen(buff)+1);
+                for(int tub = 0; tub <= maxtub; tub++){
+                     write(mtub[tub][1], buff, strlen(buff)+1);
+                }
                 break;
              }
              write(mtub[0][1], buff, strlen(buff)+1);
