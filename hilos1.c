1a57d51134dd8d8594d22d71a7b47dc32d37c2e2
SebastianSoto18 <atlasjuanse12@gmail.com>
2022-11-05 23:35:15 -0500

    añadiendo otro hilo

--- hilos1.c
+++ hilos1.c
@@ -7,14 +7,21 @@ void* funcion_hilo1(void*);
 
 
 pthread_t tid1;
+pthread_t tid2;
 int main(){
 
 	pthread_create(&tid1, NULL, funcion_hilo1, NULL);	//fork()	
+    pthread_create(&tid2, NULL, funcion_hilo2, NULL);	//fork()
 	printf("Hilo principal[%lu]\n", pthread_self());
 	pthread_join(tid1, NULL);  //wait()
+    pthread_join(tid2, NULL);  //wait()
 	return EXIT_SUCCESS;
 }
 void* funcion_hilo1(void* arg){
 	printf("Hilo1 [%lu]\n", pthread_self());
+	pthread_exit(0);
+}
+void* funcion_hilo2(void* arg){
+	printf("Hilo2 [%lu]\n", pthread_self());
 	pthread_exit(0);
 }
