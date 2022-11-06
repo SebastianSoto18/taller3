33db1228e79747f923e1094070cc9853c63169ef
SebastianSoto18 <atlasjuanse12@gmail.com>
2022-11-05 23:55:43 -0500

    hilos1 ahora pasando un dato

--- hilos1.c
+++ hilos1.c
@@ -5,21 +5,25 @@
 
 void* funcion_hilo(void*);
 
+struct nodo{
+	int a;
+	float b;
+};
+
 int main(){
 	pthread_t tid;
-	int *dato;
-	
-	dato = 10;
+	struct nodo dato;
 
-
-	pthread_create(&tid, NULL, funcion_hilo, (void*) dato);
+	dato.a = 10;
+	dato.b = 25.5;
+	pthread_create(&tid, NULL, funcion_hilo, (void*)&dato);
 	printf("Hilo principal[%lu]\n", pthread_self());
 	pthread_join(tid, NULL);
 	return EXIT_SUCCESS;
 }
 
 void* funcion_hilo(void* arg){
-	int* parametro = (int *)arg;
-	printf("Hilo [%lu] dato[%d]\n", pthread_self(), *parametro);
+	struct nodo* p = (struct nodo *)arg;
+	printf("Hilo [%lu] dato[%d | %f]\n", pthread_self(), p->a, p->b);
 	pthread_exit(0);
 }
