c58bdd64d24dfcb5978546a8af68b9e7bee4036b
SebastianSoto18 <atlasjuanse12@gmail.com>
2022-11-05 23:53:42 -0500

    hilos1 ahora pasando un dato

--- hilos1.c
+++ hilos1.c
@@ -3,28 +3,23 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-void* funcion_hilo1(void*);
+void* funcion_hilo(void*);
 
-
-pthread_t tid1;
-pthread_t tid2;
 int main(){
+	pthread_t tid;
+	int *dato;
+	
+	dato = 10;
 
-    int dato = 10;
 
-	pthread_create(&tid1, NULL, funcion_hilo1, (void *) dato);	//fork()	
-    pthread_create(&tid2, NULL, funcion_hilo2, NULL);	//fork()
+	pthread_create(&tid, NULL, funcion_hilo, (void*) dato);
 	printf("Hilo principal[%lu]\n", pthread_self());
-	pthread_join(tid1, NULL);  //wait()
-    pthread_join(tid2, NULL);  //wait()
+	pthread_join(tid, NULL);
 	return EXIT_SUCCESS;
 }
-void* funcion_hilo1(void* arg){
-    int dato = (int) arg;
-	printf("Hilo1 [%lu], y el dato es [%d]\n", pthread_self(),dato);
-	pthread_exit(0);
-}
-void* funcion_hilo2(void* arg){
-	printf("Hilo2 [%lu]\n", pthread_self());
+
+void* funcion_hilo(void* arg){
+	int* parametro = (int *)arg;
+	printf("Hilo [%lu] dato[%d]\n", pthread_self(), *parametro);
 	pthread_exit(0);
 }
