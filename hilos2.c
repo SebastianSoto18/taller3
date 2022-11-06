9dc8a0e4ce163a2d82e537e5ca48fbc5ca46f60b
SebastianSoto18 <atlasjuanse12@gmail.com>
2022-11-06 02:39:25 -0500

    hilo con sincronizacion

--- hilos2.c
+++ hilos2.c
@@ -5,24 +5,26 @@ void *funcion_hilo(void *param);
 //compilar hilos gcc -o hilos.c hilos -lpthread
 int sum ;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-
+int res;
 int main(){
     int i;
     pthread_t tid;
 
     sum=0;
-
-    printf("Creacion precia a los hijos sum=%d\n",sum);
+    res=200;
+    printf("Creacion previa a los hijos sum=%d\n",sum);
 
     pthread_create(&tid, NULL, funcion_hilo, NULL);
 
     for(i=0;i<100;i++){
         pthread_mutex_lock(&mutex);
         sum++;
+        res--;
         pthread_mutex_unlock(&mutex);
     }
     pthread_join(tid, NULL);
     printf("Suma total=%d\n",sum);
+    printf("Resta total=%d\n",res);
     pthread_mutex_destroy(&mutex);
     return 0;
 }
@@ -33,6 +35,7 @@ void *funcion_hilo(void *param){
     for(i=0;i<100;i++){
         pthread_mutex_lock(&mutex);
         sum++;
+        res--;
         pthread_mutex_unlock(&mutex);
     }
     pthread_exit(0);
