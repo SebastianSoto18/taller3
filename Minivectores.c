4c756c66d45b58e7f82ddc3a628fafe23c4da512
SebastianSoto18 <atlasjuanse12@gmail.com>
2022-11-07 04:24:23 -0500

    asasc

--- Minivectores.c
+++ Minivectores.c
@@ -20,12 +20,14 @@ int main(){
     char cantidadDeElementos[100];
     int cantidadDeElementosInt;
     int cantidadDeHilos;
+    int segmentos;
+    int digito=0;
     int bandera=1;
     char num[100];
     fscanf(archivo,"%s", &cantidadDeElementos);
     cantidadDeElementosInt=atoi(cantidadDeElementos);
     vector = (int*)malloc(sizeof(int)*cantidadDeElementosInt);
- 
+    
     for (int i = 0; i <cantidadDeElementosInt; i++)
     {
         fscanf(archivo,"%s", &num);
@@ -41,13 +43,44 @@ int main(){
         printf("Ingrese la cantidad de hilos que desea utilizar: \n");
         scanf("%d",&cantidadDeHilos);
         if(cantidadDeElementosInt%cantidadDeHilos==0){
+            segmentos=cantidadDeElementosInt/cantidadDeHilos;
             bandera=0;
         }else{
             printf("La cantidad de hilos debe ser divisible entre la cantidad de elementos\n");
         }
     }while (bandera);
+    bandera=1;
     
-     
+    pthread_t tid[cantidadDeHilos];
+    repetidosPorHilo = (int*)malloc(sizeof(int)*cantidadDeHilos);
+
+    struct parametros parametros[cantidadDeHilos];
+
+    do{
+        printf("Ingrese el digito que desea contar: \n");
+        scanf("%d",&digito);
+        if(digito<0){
+            printf("El digito debe ser mayor a 0\n");
+        }else{
+            bandera=0;
+        }
+    }while(bandera);
+
+    for (int i = 0; i < cantidadDeHilos; i++){
+        if(i==0){
+            parametros[i].inicio=0;
+            parametros[i].final=segmentos;
+            parametros[i].digito=digito;
+        }else{
+            parametros[i].inicio=i*segmentos;
+            parametros[i].final=2*parametros[i].inicio;
+            parametros[i].digito=digito;
+        }
+    }
+
+    for(int i=0;i<cantidadDeHilos;i++){
+        printf("inicio: %d final: %d digito: %d \n",parametros[i].inicio,parametros[i].final,parametros[i].digito);
+    }
     
     return 0;
 }
