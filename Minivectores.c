45186dacc72b5eecb401931ed6c57472517bc929
SebastianSoto18 <atlasjuanse12@gmail.com>
2022-11-07 04:03:59 -0500

    asdas

--- Minivectores.c
+++ Minivectores.c
@@ -18,8 +18,15 @@ int main(){
     FILE *archivo;
     archivo=fopen("test.txt","r");
     char cantidadDeElementos[2];
-
+    char num[100];
     fscanf(archivo,"%s", &cantidadDeElementos);
 
-    printf("%s",cantidadDeElementos);
+    vector = (int*)malloc(sizeof(int)*atoi(cantidadDeElementos));
+
+    for (int i = 0; i < atoi(cantidadDeElementos); i++)
+    {
+        fscanf(archivo,"%d", &num);
+        printf("%d\n ",atoi(num));
+    }
+    
 }
