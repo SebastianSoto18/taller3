9d68503489b8bcd05d82cd104756130d7ec91a0a
SebastianSoto18 <atlasjuanse12@gmail.com>
2022-11-07 04:09:00 -0500

    dfb

--- Minivectores.c
+++ Minivectores.c
@@ -18,12 +18,13 @@ int main(){
     FILE *archivo;
     archivo=fopen("test.txt","r");
     char cantidadDeElementos[2];
+    int cantidadDeElementosInt;
     char num[100];
     fscanf(archivo,"%s", &cantidadDeElementos);
-
-    vector = (int*)malloc(sizeof(int)*atoi(cantidadDeElementos));
-    printf("%d",atoi(cantidadDeElementos));
-    for (int i = 0; i <10; i++)
+    cantidadDeElementosInt=atoi(cantidadDeElementos);
+    vector = (int*)malloc(sizeof(int)*cantidadDeElementosInt);
+    printf("%d"cantidadDeElementosInt);
+    for (int i = 0; i <cantidadDeElementosInt; i++)
     {
         fscanf(archivo,"%s", &num);
         printf("%d\n ",atoi(num));
