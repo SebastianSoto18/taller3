2048604b974a0798524d512f65b8ce862d682218
SebastianSoto18 <atlasjuanse12@gmail.com>
2022-11-07 04:07:35 -0500

    dwqdq

--- Minivectores.c
+++ Minivectores.c
@@ -22,7 +22,7 @@ int main(){
     fscanf(archivo,"%s", &cantidadDeElementos);
 
     vector = (int*)malloc(sizeof(int)*atoi(cantidadDeElementos));
-
+    printf("%d",atoi(cantidadDeElementos));
     for (int i = 0; i <10; i++)
     {
         fscanf(archivo,"%s", &num);
