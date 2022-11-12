ee445541f04f9944170607e8768afd2e8c4d409d
SebastianSoto18 <atlasjuanse12@gmail.com>
2022-11-12 12:53:13 -0500

    iniciando ejercicio de la placa

---
+++ ejercicioplaca.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include<string.h>
+
+
+int main(){
+    char cadena[] = "Hola,mundo,soy,una,cadena,separa,por,comas",
+    delimitador[] = ",";
+    char *token = strtok(cadena, delimitador);
+    if(token != NULL){
+        while(token != NULL){
+            // Sólo en la primera pasamos la cadena; en las siguientes pasamos NULL
+            printf("Token: %s\n", token);
+            token = strtok(NULL, delimitador);
+        }
+    }
+}
