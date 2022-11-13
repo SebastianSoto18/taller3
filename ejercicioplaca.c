5644a428e4c1468d22eb9c8c364ba7a69ee214f0
SebastianSoto18 <atlasjuanse12@gmail.com>
2022-11-13 04:07:52 -0500

    qwe

--- ejercicioplaca.c
+++ ejercicioplaca.c
@@ -162,8 +162,8 @@ void *funcionHilo(void *param)
         for(int i=inicio;i<fin;i++){
             for(int j=1;j<columnasInt;j++){
                pthread_mutex_lock(&mutex);
-                pthread_exit(NULL);
                 placa[i][j]=(placa[i-1][j]+placa[i+1][j]+placa[i][j-1]+placa[i][j+1])/4;
+                  pthread_exit(NULL);
                 pthread_mutex_unlock(&mutex);
             }
         }
