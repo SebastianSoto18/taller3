c608aa55783bd650e14296dedba016773868ec81
SebastianSoto18 <atlasjuanse12@gmail.com>
2022-11-13 07:59:37 -0500

    asdsa

--- ejercicioplaca.c
+++ ejercicioplaca.c
@@ -144,9 +144,9 @@ int main()
                 printf("sali principal\n");
                 break;
             }
-            printf("esperando barrera principal\n");
+           
              pthread_barrier_wait(&barrera);
-             printf("saliendo de barrera principal\n");
+            
             pthread_barrier_init(&barrera, NULL, hilosmasprincipal);
             printf("esperando muro principal\n");
             pthread_barrier_wait(&muro);
