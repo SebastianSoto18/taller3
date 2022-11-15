44feee7e53d5a17f3d2553815078ed466bb7ebae
SebastianSoto18 <atlasjuanse12@gmail.com>
2022-11-15 06:53:25 -0500

    asdas

--- ejercicioplaca.c
+++ ejercicioplaca.c
@@ -93,11 +93,9 @@ int main()
     }
     pthread_barrier_wait(&barrera);
     sleep(1);
-
-     pthread_barrier_init(&barrera, NULL, hilosmasprincipal);
-
      pthread_barrier_wait(&muro);
 
+     pthread_barrier_init(&barrera, NULL, hilosmasprincipal);
      printf("\n");
      printf("\n");
      printf("\n");   
