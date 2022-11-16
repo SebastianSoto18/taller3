dafdbad51cdca097543acadfc5139dc38eff8fcd
SebastianSoto18 <atlasjuanse12@gmail.com>
2022-11-16 13:58:45 -0500

    asdas

--- estimarPi.c
+++ estimarPi.c
@@ -202,7 +202,7 @@ void  *funcion_hilo_sin_asignar(void *param){
     pthread_mutex_lock(&mutex);
     resultado+=sumaHilo;
     pthread_mutex_unlock(&mutex);
-
+    printf("calculado segmento\n"   );
     while(1){
         pthread_mutex_lock(&mutex);
         if(calculados[j]==0){
