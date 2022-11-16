b1d32c68b8bbb742faaf733b15c37c64dc5a3911
SebastianSoto18 <atlasjuanse12@gmail.com>
2022-11-16 12:47:24 -0500

    asdas

--- estimarPi.c
+++ estimarPi.c
@@ -4,6 +4,8 @@
 #include <math.h>
 
 void *funcion_hilo(void *param);
+void *funcion_hilo_sin_asignar(void *param);
+
 
 int potencia(int base,int exponente){
     int resultado=1;
@@ -16,7 +18,10 @@ int potencia(int base,int exponente){
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 float resultado=0;
-
+int  *terminos;
+int *calculados;
+int tamanoFaltante;
+ int j=0;
 struct segment{
     int inicio;
     int fin;
@@ -26,7 +31,7 @@ int main(){
     int n=0;
     int child;
     int segmento=0;
-
+    int bandera=0;
 
     do{
         printf("Ingrese hasta donde se hara la estimacion: \n");
@@ -44,7 +49,12 @@ int main(){
         if(child>n){
             printf("La cantidad de hilos debe ser menor o igual al tamaño de los elementos\n");
         }else{
-            segmento=n/child;
+            if(n%child==0){
+               segmento=n/child;
+            }else{
+                segmento=(int)n/child;
+                bandera=1;
+            }
             break;
         }
          }while(1);
@@ -52,6 +62,29 @@ int main(){
     pthread_t tid[child];
     struct segment  *d;
     d = (struct segment *)malloc(child*sizeof(struct segment));
+
+    if(bandera){
+        int cobertura=child*segmento;
+        int tamanoFaltante=n-cobertura;
+       terminos = (int *)malloc((tamanoFaltante)*sizeof(int));
+     calculados = (int *)malloc((tamanoFaltante)*sizeof(int));
+
+        for(int i=tamanoFaltante;i<n;i++){
+            terminos[i]=i;
+            calculados[i]=0;
+        }
+
+    for(int i=0;i<child;i++){
+            if(i==0){
+                d[i].inicio=0;
+            }else{
+                d[i].inicio=i*segmento;
+            }
+            d[i].fin=d[i].inicio+segmento;
+            pthread_create(&tid[i],NULL,funcion_hilo_sin_asignar,(void *)&d[i]);
+    }
+
+    }else{
 
     for(int i=0;i<child;i++){
        if(i==0){
@@ -61,6 +94,7 @@ int main(){
        }
        d[i].fin=d[i].inicio+segmento;
        pthread_create(&tid[i],NULL,funcion_hilo,(void *)&d[i]);
+    }
     }
 
     for(int i=0;i<child;i++){
@@ -91,18 +125,63 @@ void *funcion_hilo(void *param){
         }
 
         if(signoSerie==1){
-            printf("positivo\n");
            sumaHilo=sumaHilo + (double)1/(2*i+1);
         }else{
-            printf("negativo\n");
             sumaHilo=sumaHilo - (double)1/(2*i+1);
         }
     }
 
     pthread_mutex_lock(&mutex);
     resultado+=sumaHilo;
-    printf("Resultado: %f\n",sumaHilo);
     pthread_mutex_unlock(&mutex);
 
+    pthread_exit(0);
+}
+
+void  *funcion_hilo_sin_asignar(void *param){
+    struct segment *d = (struct segment *)param;
+    int inicio=d->inicio;
+    int fin=d->fin;
+    int signoSerie=1;
+    double  sumaHilo=0;
+
+    for(int i=inicio;i<fin;i++){
+        if(i==0){
+            signoSerie=1;
+        }else{
+            signoSerie=potencia(-1,i);
+        }
+
+        if(signoSerie==1){
+           sumaHilo=sumaHilo + (double)1/(2*i+1);
+        }else{
+            sumaHilo=sumaHilo - (double)1/(2*i+1);
+        }
+    }
+
+    pthread_mutex_lock(&mutex);
+    resultado+=sumaHilo;
+    pthread_mutex_unlock(&mutex);
+
+    while(1){
+        pthread_mutex_lock(&mutex);
+        if(calculados[j]==0){
+            calculados[j]=1;
+           signoSerie=potencia(-1,terminos[j]);
+            if(signoSerie==1){
+                resultado=resultado + (double)1/(2*terminos[j]+1);
+            }else{
+                resultado=resultado - (double)1/(2*terminos[j]+1);
+            }
+            j++;
+            }else{
+            j++;
+            }
+            if(j==tamanoFaltante){
+                break;
+            }
+            pthread_mutex_unlock(&mutex);
+        }
+    
     pthread_exit(0);
 }
