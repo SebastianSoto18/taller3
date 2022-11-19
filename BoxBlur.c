167ca87e6b3fd67f684d0b89854900cbf8907cd2
SebastianSoto18 <atlasjuanse12@gmail.com>
2022-11-19 12:39:32 -0500

    asdas

--- BoxBlur.c
+++ BoxBlur.c
@@ -25,8 +25,9 @@ int *IsSobrante;
 int main(){
     FILE *file;
    file=fopen("test4.txt","r");
-    f=fscanf(file,"%d",&f);
-    c=fscanf(file,"%d",&c);
+    fscanf(file,"%d",&f);
+   fscanf(file,"%d",&f);
+
     printf("f: %d c: %d\n",f,c);
     int canHilos=0;
     imagen=(int**)malloc(f*sizeof(int*));
