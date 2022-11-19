d5fc4486b21a9b97d446df9cf0b806841bfa2c01
SebastianSoto18 <atlasjuanse12@gmail.com>
2022-11-19 12:24:41 -0500

    uih

--- BoxBlur.c
+++ BoxBlur.c
@@ -26,25 +26,25 @@ int main(){
     FILE *archivo;
     archivo=fopen("test4.txt","r");
     int canHilos=0;
-    filas=fscanf(archivo,"%d",&filas);
-    columnas=fscanf(archivo,"%d",&columnas);
-    printf("filas: %d columnas: %d\n",filas,columnas);
-    imagen=(int**)malloc(filas*sizeof(int*));
-    imagenConFiltro=(int**)malloc(filas*sizeof(int*));
+    f=fscanf(archivo,"%d",&f);
+    c=fscanf(archivo,"%d",&c);
+    printf("f: %d c: %d\n",f,c);
+    imagen=(int**)malloc(f*sizeof(int*));
+    imagenConFiltro=(int**)malloc(f*sizeof(int*));
 
-    for(int i=0;i<filas;i++){
-        imagen[i]=(int*)malloc(columnas*sizeof(int));
-        imagenConFiltro[i]=(int*)malloc(columnas*sizeof(int));
+    for(int i=0;i<f;i++){
+        imagen[i]=(int*)malloc(c*sizeof(int));
+        imagenConFiltro[i]=(int*)malloc(c*sizeof(int));
     }
 
-    for(int i=0;i<filas;i++){
-        for(int j=0;j<columnas;j++){
+    for(int i=0;i<f;i++){
+        for(int j=0;j<c;j++){
             fscanf(archivo,"%d",&imagen[i][j]);
         }
     }
 
-       for(int i=0;i<filas;i++){
-        for(int j=0;j<columnas;j++){
+       for(int i=0;i<f;i++){
+        for(int j=0;j<c;j++){
            printf("%d ",imagen[i][j]);
         }
         printf("\n");
