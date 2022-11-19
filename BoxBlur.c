628fc9939ff9842486f08126614cfd2317d0c5e5
SebastianSoto18 <atlasjuanse12@gmail.com>
2022-11-19 12:14:48 -0500

    asdas

--- BoxBlur.c
+++ BoxBlur.c
@@ -18,5 +18,43 @@ pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 int **imagen;
 int **imagenConFiltro;
-int canSobrantes ;
+int canSobrantes=0 ;
 int columnas=0;
+int *IsSobrante;
+
+
+int main(){
+    FILE *archivo;
+    archivo=fopen("test4.txt","r");
+    int filas=0;
+    int canHilos=0;
+    filas=fscanf(archivo,"%d",&filas);
+    columnas=fscanf(archivo,"%d",&columnas);
+
+    imagen=(int**)malloc(filas*sizeof(int*));
+    imagenConFiltro=(int**)malloc(filas*sizeof(int*));
+
+    for(int i=0;i<filas;i++){
+        imagen[i]=(int*)malloc(columnas*sizeof(int));
+        imagenConFiltro[i]=(int*)malloc(columnas*sizeof(int));
+    }
+
+    for(int i=0;i<filas;i++){
+        for(int j=0;j<columnas;j++){
+            fscanf(archivo,"%d",&imagen[i][j]);
+        }
+    }
+
+       for(int i=0;i<filas;i++){
+        for(int j=0;j<columnas;j++){
+           printf("%d ",imagen[i][j]);
+        }
+        printf("\n");
+    }
+
+    fclose(archivo);
+    free(imagen);
+    free(imagenConFiltro);
+    free(IsSobrante);
+
+}
